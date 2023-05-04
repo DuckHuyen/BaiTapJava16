6cf6c860b6bf85979d8f6f968164fcf9bda61f4e
DuckHuyen <huyentd.k43@c3nguyendu,edu.vn>
2023-05-04 19:07:36 +0700

    hoan thanh ngay 5/4

---
+++ BaiThucHanhLab4/src/student.java
@@ -0,0 +1,13 @@
+
+public class student {
+	char[] id = new char[10];
+	char[] maSinhVien = new char[10];
+	String ten;
+	public student(char[] id, char[] maSinhVien, String ten) {
+		super();
+		this.id = id;
+		this.maSinhVien = maSinhVien;
+		this.ten = ten;
+	}
+	
+}
