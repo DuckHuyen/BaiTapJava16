d1fb64f50b2ddc01ada1a9f00049d625acf7f622
DuckHuyen <huyentd.k43@c3nguyendu,edu.vn>
2023-05-09 22:41:03 +0700

    5/9/2023

---
+++ BaiThucHanhLab4/src/Lists/Arraylistdemo.java
@@ -0,0 +1,17 @@
+package Lists;
+
+import java.util.ArrayList;
+
+import student.Students;
+
+
+public class Arraylistdemo {
+	
+
+	public static void main(String[] args) {
+		ArrayList<Students> std = new ArrayList<>();
+		Students.menu(std);
+		
+	}
+
+}
